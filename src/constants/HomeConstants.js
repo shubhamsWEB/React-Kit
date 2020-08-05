7b5adf0c80104d1572abfa7ff9497fe48c97f1c3
unknown <shubhama664@gmail.com>
2020-08-05 10:21:39 +0530

    Update HomeConstants.js

--- src/constants/HomeConstants.js
+++ src/constants/HomeConstants.js
@@ -1,6 +1,6 @@
 export const WELCOME_HEADING = 'React Enterprise Starter Kit';
 export const IMG_ALT = 'React-Enterprise-Starter-Kit';
-export const REPO_URL = 'https://github.com/anandgupta193/react-enterprise-starter-kit';
+export const REPO_URL = 'https://github.com/shubhamsWEB/React-Kit';
 export const GITHUB_FORK_TEXT = 'Fork on Github';
 export const LICENCE_BADGE = 'https://img.shields.io/github/license/anandgupta193/react-enterprise-starter-kit?style=for-the-badge';
 export const STARS_BADGE = 'https://img.shields.io/github/stars/anandgupta193/react-enterprise-starter-kit?style=for-the-badge';
